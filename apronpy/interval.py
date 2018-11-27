9706a4914fbba4411f5f8899e1de20a1f30f661d
Caterina Urban <caterina.urban@gmail.com>
2018-11-27 17:09:45 +0100

    APRON intervals

--- apronpy/interval.py
+++ apronpy/interval.py
@@ -70,6 +70,44 @@ class PyInterval(metaclass=ABCMeta):
     def __repr__(self):
         return str(self.interval.contents)
 
+    """Tests"""
+    
+    def is_bottom(self):
+        return bool(libapron.ap_interval_is_bottom(self))
+    
+    def __lt__(self, other: 'PyInterval'):
+        assert isinstance(other, PyInterval)
+        return libapron.ap_interval_cmp(self, other) == -1
+
+    def __le__(self, other: 'PyInterval'):
+        assert isinstance(other, PyInterval)
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __eq__(self, other):
+        assert isinstance(other, PyInterval)
+        return libapron.ap_interval_cmp(self, other) == 0
+
+    def __ne__(self, other):
+        assert isinstance(other, PyInterval)
+        return not self.__eq__(other)
+
+    def __ge__(self, other):
+        assert isinstance(other, PyInterval)
+        return self.__gt__(other) or self.__eq__(other)
+
+    def __gt__(self, other):
+        assert isinstance(other, PyInterval)
+        return libapron.ap_interval_cmp(self, other) == 1
+
+    def is_top(self):
+        return bool(libapron.ap_interval_is_top(self))
+
+    """Other Operations"""
+
+    def __neg__(self):
+        libapron.ap_interval_neg(self, self)
+        return self
+
 
 libapron.ap_interval_alloc.restype = POINTER(Interval)
 libapron.ap_interval_set_int.argtypes = [PyInterval, c_int, c_int]
@@ -77,6 +115,10 @@ libapron.ap_interval_set_double.argtypes = [PyInterval, c_double, c_double]
 libapron.ap_interval_set_mpq.argtypes = [PyInterval, PyMPQ, PyMPQ]
 libapron.ap_interval_set_mpfr.argtypes = [PyInterval, PyMPFR, PyMPFR]
 libapron.ap_interval_set_scalar.argtypes = [PyInterval, PyScalar, PyScalar]
+libapron.ap_interval_is_bottom.argtypes = [PyInterval]
+libapron.ap_interval_is_top.argtypes = [PyInterval]
+libapron.ap_interval_cmp.argtypes = [PyInterval, PyInterval]
+libapron.ap_interval_neg.argtypes = [PyInterval, PyInterval]
 
 
 class PyDoubleInterval(PyInterval):
--- tests/test_interval.py
+++ tests/test_interval.py
@@ -33,38 +33,64 @@ class TestDoubleInterval(unittest.TestCase):
         self.assertEqual(str(PyDoubleInterval.init_top()), '[-inf,inf]')
         self.assertEqual(str(PyDoubleInterval.init_bottom()), '[1.0,-1.0]')
 
-    # def test_infty(self):
-    #     self.assertEqual(PyDoubleScalar(9).infty(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(-9).infty(), -1)
-    #     self.assertEqual(PyDoubleScalar.init_infty(0).infty(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(9).infty(), 1)
-    #
-    # def test_cmp(self):
-    #     self.assertTrue(PyDoubleScalar(0.5) < PyDoubleScalar(9))
-    #     self.assertTrue(PyDoubleScalar(9) == PyDoubleScalar(9))
-    #     self.assertTrue(PyDoubleScalar(9) > PyDoubleScalar(0.5))
-    #
-    # def test_sign(self):
-    #     self.assertEqual(PyDoubleScalar(-9).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar(c_double(-9)).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar(9).sign(), 1)
-    #     self.assertEqual(PyDoubleScalar(c_double(9)).sign(), 1)
-    #     self.assertEqual(PyDoubleScalar(0).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar(c_double(0)).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(-9).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar.init_infty(0).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(9).sign(), 1)
-    #
-    # def test_neg(self):
-    #     self.assertEqual(-PyDoubleScalar(-9), PyDoubleScalar(9))
-    #     self.assertEqual(-PyDoubleScalar(c_double(-9)), PyDoubleScalar(c_double(9)))
-    #     self.assertEqual(-PyDoubleScalar(9), PyDoubleScalar(-9))
-    #     self.assertEqual(-PyDoubleScalar(c_double(9)), PyDoubleScalar(c_double(-9)))
-    #     self.assertEqual(-PyDoubleScalar(0), PyDoubleScalar(0))
-    #     self.assertEqual(-PyDoubleScalar(c_double(0)), PyDoubleScalar(c_double(0)))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(-9), PyDoubleScalar.init_infty(9))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(0), PyDoubleScalar.init_infty(0))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(9), PyDoubleScalar.init_infty(-9))
+    def test_is_bottom(self):
+        self.assertFalse(PyDoubleInterval().is_bottom())
+        self.assertFalse(PyDoubleInterval(0, 0).is_bottom())
+        self.assertFalse(PyDoubleInterval(0.0, 0.0).is_bottom())
+        self.assertFalse(PyDoubleInterval(c_double(0), c_double(0)).is_bottom())
+        self.assertFalse(PyDoubleInterval(c_double(0.0), c_double(0.0)).is_bottom())
+        self.assertFalse(PyDoubleInterval(PyDoubleScalar(0), PyDoubleScalar(0)).is_bottom())
+        self.assertFalse(PyDoubleInterval(PyDoubleScalar(0.0), PyDoubleScalar(0.0)).is_bottom())
+        self.assertFalse(PyDoubleInterval(-0.5, 0.5).is_bottom())
+        self.assertFalse(PyDoubleInterval(c_double(-0.5), c_double(0.5)).is_bottom())
+        self.assertFalse(PyDoubleInterval(PyDoubleScalar(-0.5), PyDoubleScalar(0.5)).is_bottom())
+        self.assertFalse(PyDoubleInterval.init_top().is_bottom())
+        self.assertTrue(PyDoubleInterval.init_bottom().is_bottom())
+    
+    def test_cmp(self):
+        self.assertTrue(PyDoubleInterval.init_bottom() < PyDoubleInterval())
+        self.assertFalse(PyDoubleInterval() < PyDoubleInterval.init_bottom())
+        self.assertTrue(PyDoubleInterval() < PyDoubleInterval(-0.5, 0.5))
+        self.assertFalse(PyDoubleInterval(-0.5, 0.5) < PyDoubleInterval())
+        self.assertTrue(PyDoubleInterval(-0.5, 0.5) < PyDoubleInterval.init_top())
+        self.assertFalse(PyDoubleInterval.init_top() < PyDoubleInterval(-0.5, 0.5))
+        self.assertFalse(PyDoubleInterval.init_bottom() == PyDoubleInterval())
+        self.assertTrue(PyDoubleInterval() == PyDoubleInterval(0, 0))
+        self.assertFalse(PyDoubleInterval() == PyDoubleInterval(-0.5, 0.5))
+        self.assertTrue(
+            PyDoubleInterval(-0.5, 0.5) == PyDoubleInterval(c_double(-0.5), c_double(0.5))
+        )
+        self.assertFalse(PyDoubleInterval(-0.5, 0.5) == PyDoubleInterval.init_top())
+        self.assertFalse(PyDoubleInterval.init_bottom() > PyDoubleInterval())
+        self.assertTrue(PyDoubleInterval() > PyDoubleInterval.init_bottom())
+        self.assertFalse(PyDoubleInterval() > PyDoubleInterval(-0.5, 0.5))
+        self.assertTrue(PyDoubleInterval(-0.5, 0.5) > PyDoubleInterval())
+        self.assertFalse(PyDoubleInterval(-0.5, 0.5) > PyDoubleInterval.init_top())
+        self.assertTrue(PyDoubleInterval.init_top() > PyDoubleInterval(-0.5, 0.5))
+    
+    def test_is_top(self):
+        self.assertFalse(PyDoubleInterval().is_top())
+        self.assertFalse(PyDoubleInterval(0, 0).is_top())
+        self.assertFalse(PyDoubleInterval(0.0, 0.0).is_top())
+        self.assertFalse(PyDoubleInterval(c_double(0), c_double(0)).is_top())
+        self.assertFalse(PyDoubleInterval(c_double(0.0), c_double(0.0)).is_top())
+        self.assertFalse(PyDoubleInterval(PyDoubleScalar(0), PyDoubleScalar(0)).is_top())
+        self.assertFalse(PyDoubleInterval(PyDoubleScalar(0.0), PyDoubleScalar(0.0)).is_top())
+        self.assertFalse(PyDoubleInterval(-0.5, 0.5).is_top())
+        self.assertFalse(PyDoubleInterval(c_double(-0.5), c_double(0.5)).is_top())
+        self.assertFalse(PyDoubleInterval(PyDoubleScalar(-0.5), PyDoubleScalar(0.5)).is_top())
+        self.assertTrue(PyDoubleInterval.init_top().is_top())
+        self.assertFalse(PyDoubleInterval.init_bottom().is_top())
+
+    def test_neg(self):
+        self.assertEqual(-PyDoubleInterval.init_bottom(), PyDoubleInterval.init_bottom())
+        self.assertEqual(-PyDoubleInterval(-1, 2), PyDoubleInterval(-2, 1))
+        self.assertEqual(-PyDoubleInterval(), PyDoubleInterval(0, 0))
+        self.assertEqual(
+            -PyDoubleInterval(-0.5, 0.5), PyDoubleInterval(c_double(-0.5), c_double(0.5))
+        )
+        self.assertEqual(-PyDoubleInterval(1, 2), PyDoubleInterval(-2, -1))
+        self.assertEqual(-PyDoubleInterval.init_top(), PyDoubleInterval.init_top())
 
 
 class TestMPQInterval(unittest.TestCase):
@@ -81,38 +107,58 @@ class TestMPQInterval(unittest.TestCase):
         self.assertEqual(str(PyMPQInterval.init_top()), '[-1/0,1/0]')
         self.assertEqual(str(PyMPQInterval.init_bottom()), '[1,-1]')
 
-    # def test_infty(self):
-    #     self.assertEqual(PyDoubleScalar(9).infty(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(-9).infty(), -1)
-    #     self.assertEqual(PyDoubleScalar.init_infty(0).infty(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(9).infty(), 1)
-    #
-    # def test_cmp(self):
-    #     self.assertTrue(PyDoubleScalar(0.5) < PyDoubleScalar(9))
-    #     self.assertTrue(PyDoubleScalar(9) == PyDoubleScalar(9))
-    #     self.assertTrue(PyDoubleScalar(9) > PyDoubleScalar(0.5))
-    #
-    # def test_sign(self):
-    #     self.assertEqual(PyDoubleScalar(-9).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar(c_double(-9)).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar(9).sign(), 1)
-    #     self.assertEqual(PyDoubleScalar(c_double(9)).sign(), 1)
-    #     self.assertEqual(PyDoubleScalar(0).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar(c_double(0)).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(-9).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar.init_infty(0).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(9).sign(), 1)
-    #
-    # def test_neg(self):
-    #     self.assertEqual(-PyDoubleScalar(-9), PyDoubleScalar(9))
-    #     self.assertEqual(-PyDoubleScalar(c_double(-9)), PyDoubleScalar(c_double(9)))
-    #     self.assertEqual(-PyDoubleScalar(9), PyDoubleScalar(-9))
-    #     self.assertEqual(-PyDoubleScalar(c_double(9)), PyDoubleScalar(c_double(-9)))
-    #     self.assertEqual(-PyDoubleScalar(0), PyDoubleScalar(0))
-    #     self.assertEqual(-PyDoubleScalar(c_double(0)), PyDoubleScalar(c_double(0)))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(-9), PyDoubleScalar.init_infty(9))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(0), PyDoubleScalar.init_infty(0))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(9), PyDoubleScalar.init_infty(-9))
+    def test_is_bottom(self):
+        self.assertFalse(PyMPQInterval().is_bottom())
+        self.assertFalse(PyMPQInterval(0, 0).is_bottom())
+        self.assertFalse(PyMPQInterval(0, 0, 1, 1).is_bottom())
+        self.assertFalse(PyMPQInterval(PyMPQScalar(0), PyMPQScalar(0)).is_bottom())
+        self.assertFalse(PyMPQInterval(PyMPQScalar(0, 1), PyMPQScalar(0, 1)).is_bottom())
+        self.assertFalse(PyMPQInterval(-1, 1, 2, 2).is_bottom())
+        self.assertFalse(PyMPQInterval(PyMPQ(-1, 2), PyMPQ(1, 2)).is_bottom())
+        self.assertFalse(PyMPQInterval(PyMPQScalar(-1, 2), PyMPQScalar(1, 2)).is_bottom())
+        self.assertFalse(PyMPQInterval.init_top().is_bottom())
+        self.assertTrue(PyMPQInterval.init_bottom().is_bottom())
+
+    def test_cmp(self):
+        self.assertTrue(PyMPQInterval.init_bottom() < PyMPQInterval())
+        self.assertFalse(PyMPQInterval() < PyMPQInterval.init_bottom())
+        self.assertTrue(PyMPQInterval() < PyMPQInterval(-1, 1, 2, 2))
+        self.assertFalse(PyMPQInterval(-1, 1, 2, 2) < PyMPQInterval())
+        self.assertTrue(PyMPQInterval(-1, 1, 2, 2) < PyMPQInterval.init_top())
+        self.assertFalse(PyMPQInterval.init_top() < PyMPQInterval(-1, 1, 2, 2))
+        self.assertFalse(PyMPQInterval.init_bottom() == PyMPQInterval())
+        self.assertTrue(PyMPQInterval() == PyMPQInterval(0, 0))
+        self.assertFalse(PyMPQInterval() == PyMPQInterval(-1, 1, 2, 2))
+        self.assertTrue(PyMPQInterval(-1, 1, 2, 2) == PyMPQInterval(PyMPQ(-1, 2), PyMPQ(1, 2)))
+        self.assertFalse(PyMPQInterval(-1, 1, 2, 2) == PyMPQInterval.init_top())
+        self.assertFalse(PyMPQInterval.init_bottom() > PyMPQInterval())
+        self.assertTrue(PyMPQInterval() > PyMPQInterval.init_bottom())
+        self.assertFalse(PyMPQInterval() > PyMPQInterval(-1, 1, 2, 2))
+        self.assertTrue(PyMPQInterval(-1, 1, 2, 2) > PyMPQInterval())
+        self.assertFalse(PyMPQInterval(-1, 1, 2, 2) > PyMPQInterval.init_top())
+        self.assertTrue(PyMPQInterval.init_top() > PyMPQInterval(-1, 1, 2, 2))
+    
+    def test_is_top(self):
+        self.assertFalse(PyMPQInterval().is_top())
+        self.assertFalse(PyMPQInterval(0, 0).is_top())
+        self.assertFalse(PyMPQInterval(0, 0, 1, 1).is_top())
+        self.assertFalse(PyMPQInterval(PyMPQScalar(0), PyMPQScalar(0)).is_top())
+        self.assertFalse(PyMPQInterval(PyMPQScalar(0, 1), PyMPQScalar(0, 1)).is_top())
+        self.assertFalse(PyMPQInterval(-1, 1, 2, 2).is_top())
+        self.assertFalse(PyMPQInterval(PyMPQ(-1, 2), PyMPQ(1, 2)).is_top())
+        self.assertFalse(PyMPQInterval(PyMPQScalar(-1, 2), PyMPQScalar(1, 2)).is_top())
+        self.assertTrue(PyMPQInterval.init_top().is_top())
+        self.assertFalse(PyMPQInterval.init_bottom().is_top())
+    
+    def test_neg(self):
+        self.assertEqual(-PyMPQInterval.init_bottom(), PyMPQInterval.init_bottom())
+        self.assertEqual(-PyMPQInterval(-1, 2), PyMPQInterval(-2, 1))
+        self.assertEqual(-PyMPQInterval(), PyMPQInterval(0, 0))
+        self.assertEqual(-PyMPQInterval(-1, 1, 2, 2), PyMPQInterval(-PyMPQ(1, 2), PyMPQ(1, 2)))
+        self.assertEqual(-PyMPQInterval(1, 1, 2, 2), PyMPQInterval(-PyMPQ(1, 2), -PyMPQ(1, 2)))
+        self.assertEqual(-PyMPQInterval(1, 1, 2, 2), PyMPQInterval(PyMPQ(-1, 2), PyMPQ(-1, 2)))
+        self.assertEqual(-PyMPQInterval(1, 2), PyMPQInterval(-2, -1))
+        self.assertEqual(-PyMPQInterval.init_top(), PyMPQInterval.init_top())
 
 
 class TestMPFRInterval(unittest.TestCase):
@@ -128,38 +174,54 @@ class TestMPFRInterval(unittest.TestCase):
         self.assertEqual(str(PyMPFRInterval.init_top()), '[-inf,inf]')
         self.assertEqual(str(PyMPFRInterval.init_bottom()), '[1.0,-1.0]')
 
-    # def test_infty(self):
-    #     self.assertEqual(PyDoubleScalar(9).infty(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(-9).infty(), -1)
-    #     self.assertEqual(PyDoubleScalar.init_infty(0).infty(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(9).infty(), 1)
-    #
-    # def test_cmp(self):
-    #     self.assertTrue(PyDoubleScalar(0.5) < PyDoubleScalar(9))
-    #     self.assertTrue(PyDoubleScalar(9) == PyDoubleScalar(9))
-    #     self.assertTrue(PyDoubleScalar(9) > PyDoubleScalar(0.5))
-    #
-    # def test_sign(self):
-    #     self.assertEqual(PyDoubleScalar(-9).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar(c_double(-9)).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar(9).sign(), 1)
-    #     self.assertEqual(PyDoubleScalar(c_double(9)).sign(), 1)
-    #     self.assertEqual(PyDoubleScalar(0).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar(c_double(0)).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(-9).sign(), -1)
-    #     self.assertEqual(PyDoubleScalar.init_infty(0).sign(), 0)
-    #     self.assertEqual(PyDoubleScalar.init_infty(9).sign(), 1)
-    #
-    # def test_neg(self):
-    #     self.assertEqual(-PyDoubleScalar(-9), PyDoubleScalar(9))
-    #     self.assertEqual(-PyDoubleScalar(c_double(-9)), PyDoubleScalar(c_double(9)))
-    #     self.assertEqual(-PyDoubleScalar(9), PyDoubleScalar(-9))
-    #     self.assertEqual(-PyDoubleScalar(c_double(9)), PyDoubleScalar(c_double(-9)))
-    #     self.assertEqual(-PyDoubleScalar(0), PyDoubleScalar(0))
-    #     self.assertEqual(-PyDoubleScalar(c_double(0)), PyDoubleScalar(c_double(0)))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(-9), PyDoubleScalar.init_infty(9))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(0), PyDoubleScalar.init_infty(0))
-    #     self.assertEqual(-PyDoubleScalar.init_infty(9), PyDoubleScalar.init_infty(-9))
+    def test_is_bottom(self):
+        self.assertFalse(PyMPFRInterval(0, 0).is_bottom())
+        self.assertFalse(PyMPFRInterval(0.0, 0.0).is_bottom())
+        self.assertFalse(PyMPFRInterval(PyMPFRScalar(0), PyMPFRScalar(0)).is_bottom())
+        self.assertFalse(PyMPFRInterval(PyMPFRScalar(0.0), PyMPFRScalar(0.0)).is_bottom())
+        self.assertFalse(PyMPFRInterval(-0.5, 0.5).is_bottom())
+        self.assertFalse(PyMPFRInterval(PyMPFR(-0.5), PyMPFR(0.5)).is_bottom())
+        self.assertFalse(PyMPFRInterval(PyMPFRScalar(-0.5), PyMPFRScalar(0.5)).is_bottom())
+        self.assertFalse(PyMPFRInterval.init_top().is_bottom())
+        self.assertTrue(PyMPFRInterval.init_bottom().is_bottom())
+        
+    def test_cmp(self):
+        self.assertTrue(PyMPFRInterval.init_bottom() < PyMPFRInterval(0, 0))
+        self.assertFalse(PyMPFRInterval(0, 0) < PyMPFRInterval.init_bottom())
+        self.assertTrue(PyMPFRInterval(0, 0) < PyMPFRInterval(-0.5, 0.5))
+        self.assertFalse(PyMPFRInterval(-0.5, 0.5) < PyMPFRInterval(0, 0))
+        self.assertTrue(PyMPFRInterval(-0.5, 0.5) < PyMPFRInterval.init_top())
+        self.assertFalse(PyMPFRInterval.init_top() < PyMPFRInterval(-0.5, 0.5))
+        self.assertFalse(PyMPFRInterval.init_bottom() == PyMPFRInterval(0, 0))
+        self.assertTrue(PyMPFRInterval(0, 0) == PyMPFRInterval(0, 0))
+        self.assertFalse(PyMPFRInterval(0, 0) == PyMPFRInterval(-0.5, 0.5))
+        self.assertTrue(PyMPFRInterval(-0.5, 0.5) == PyMPFRInterval(PyMPFR(-0.5), PyMPFR(0.5)))
+        self.assertFalse(PyMPFRInterval(-0.5, 0.5) == PyMPFRInterval.init_top())
+        self.assertFalse(PyMPFRInterval.init_bottom() > PyMPFRInterval(0, 0))
+        self.assertTrue(PyMPFRInterval(0, 0) > PyMPFRInterval.init_bottom())
+        self.assertFalse(PyMPFRInterval(0, 0) > PyMPFRInterval(-0.5, 0.5))
+        self.assertTrue(PyMPFRInterval(-0.5, 0.5) > PyMPFRInterval(0, 0))
+        self.assertFalse(PyMPFRInterval(-0.5, 0.5) > PyMPFRInterval.init_top())
+        self.assertTrue(PyMPFRInterval.init_top() > PyMPFRInterval(-0.5, 0.5))
+        
+    def test_is_top(self):
+        self.assertFalse(PyMPFRInterval(0, 0).is_top())
+        self.assertFalse(PyMPFRInterval(0.0, 0.0).is_top())
+        self.assertFalse(PyMPFRInterval(PyMPFRScalar(0), PyMPFRScalar(0)).is_top())
+        self.assertFalse(PyMPFRInterval(PyMPFRScalar(0.0), PyMPFRScalar(0.0)).is_top())
+        self.assertFalse(PyMPFRInterval(-0.5, 0.5).is_top())
+        self.assertFalse(PyMPFRInterval(PyMPFR(-0.5), PyMPFR(0.5)).is_top())
+        self.assertFalse(PyMPFRInterval(PyMPFRScalar(-0.5), PyMPFRScalar(0.5)).is_top())
+        self.assertTrue(PyMPFRInterval.init_top().is_top())
+        self.assertFalse(PyMPFRInterval.init_bottom().is_top())
+    
+    def test_neg(self):
+        self.assertEqual(-PyMPFRInterval.init_bottom(), PyMPFRInterval.init_bottom())
+        self.assertEqual(-PyMPFRInterval(-1, 2), PyMPFRInterval(-2, 1))
+        self.assertEqual(-PyMPFRInterval(0, 0), PyMPFRInterval(0, 0))
+        self.assertEqual(-PyMPFRInterval(-0.5, 0.5), PyMPFRInterval(PyMPFR(-0.5), PyMPFR(0.5)))
+        self.assertEqual(-PyMPFRInterval(1, 2), PyMPFRInterval(-2, -1))
+        self.assertEqual(-PyMPFRInterval.init_top(), PyMPFRInterval.init_top())
 
 
 if __name__ == '__main__':
