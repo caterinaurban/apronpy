06fd7a50faf9dc5c369f129d7b7d00ee710d814e
Caterina Urban <caterina.urban@gmail.com>
2019-02-21 11:46:28 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='apronpy',
-    version='0.2.1',
+    version='0.2.2',
     author='Caterina Urban',
     author_email='caterina.urban@gmail.com',
     description='Python Interface for the APRON Numerical Abstract Domain Library',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/caterinaurban/apronpy',
     packages=find_packages(),
 )
