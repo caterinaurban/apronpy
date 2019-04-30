075f82b364fe56ce8d5f4917e6e6dda381538e12
Caterina Urban <caterina.urban@gmail.com>
2019-04-30 08:02:47 +0200

    APRON tree constraints

--- apronpy/tcons1.py
+++ apronpy/tcons1.py
@@ -97,6 +97,58 @@ class TCons1Array(Structure):
         ('env', POINTER(Environment))
     ]
 
+    def __repr__(self):
+        def precendence(texpr0):
+            op_precedence = {
+                TexprOp.AP_TEXPR_ADD: 1,
+                TexprOp.AP_TEXPR_SUB: 1,
+                TexprOp.AP_TEXPR_MUL: 2,
+                TexprOp.AP_TEXPR_DIV: 2,
+                TexprOp.AP_TEXPR_MOD: 2,
+                TexprOp.AP_TEXPR_POW: 3,
+                TexprOp.AP_TEXPR_NEG: 4,
+                TexprOp.AP_TEXPR_CAST: 5,
+                TexprOp.AP_TEXPR_SQRT: 5
+            }
+            if texpr0.discr == TexprDiscr.AP_TEXPR_CST or texpr0.discr == TexprDiscr.AP_TEXPR_DIM:
+                return op_precedence[TexprOp.AP_TEXPR_NEG]
+            return op_precedence[texpr0.val.node.contents.op]
+
+        def do(texpr0, env):
+            if texpr0.discr == TexprDiscr.AP_TEXPR_CST:
+                return '{}'.format(texpr0.val.cst)
+            elif texpr0.discr == TexprDiscr.AP_TEXPR_DIM:
+                return '{}'.format(env.var_of_dim[texpr0.val.dim.value].decode('utf-8'))
+            else:  # texpr0.discr == TexprDiscr.AP_TEXPR_NODE
+                prec = precendence(texpr0)
+                prec_a = precendence(texpr0.val.node.contents.exprA.contents)
+                if prec_a < prec:
+                    expr_a = '(' + do(texpr0.val.node.contents.exprA.contents, env) + ')'
+                else:
+                    expr_a = do(texpr0.val.node.contents.exprA.contents, env)
+                op = texpr0.val.node.contents.op
+                if texpr0.val.node.contents.exprB:  # binary operation
+                    prec_b = precendence(texpr0.val.node.contents.exprB.contents)
+                    if prec_b <= prec:
+                        expr_b = '(' + do(texpr0.val.node.contents.exprB.contents, env) + ')'
+                    else:
+                        expr_b = do(texpr0.val.node.contents.exprB.contents, env)
+                    return '{} {} {}'.format(expr_a, repr(TexprOp(op)), expr_b)
+                else:
+                    return '{} {}'.format(repr(TexprOp(op)), expr_a)
+
+        env = self.env.contents
+        array = list()
+        for i in range(self.tcons0_array.size):
+            constyp = ConsTyp(self.tcons0_array.p[i].constyp)
+            scalar = self.tcons0_array.p[i].scalar
+            result = do(self.tcons0_array.p[i].texpr0.contents, env).replace('+ -', '- ')
+            if scalar:
+                array.append('{} {} {}'.format(result, repr(constyp), scalar.contents))
+            else:
+                array.append('{} {} 0'.format(result, repr(constyp)))
+        return '[' + ' , '.join(array) + ']'
+
 
 class PyTcons1:
 
@@ -189,6 +241,9 @@ class PyTcons1Array:
     def from_param(argument):
         assert isinstance(argument, PyTcons1Array)
         return argument
+
+    def __repr__(self):
+        return '{}'.format(self.tcons1array)
 
 
 libapron.ap_tcons1_array_make.argtypes = [POINTER(Environment), c_size_t]
